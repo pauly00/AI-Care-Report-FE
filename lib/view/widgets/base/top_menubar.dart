869db281c273ab2ea936ef7294192fab2f5fd628
이유진 <jenny7732@naver.com>
2024-10-16 15:01:24 +0900

    feat: 상단 하단 메뉴바 수정

---
+++ lib/view/widgets/base/top_menubar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class TopMenubar extends StatelessWidget {
+  final String title;
+  final bool showBackButton; // 뒤로가기 버튼 표시 여부
+
+  const TopMenubar({
+    super.key,
+    required this.title,
+    this.showBackButton = false, // 기본값은 false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFFFF6F6), // 배경색 설정
+      padding: const EdgeInsets.symmetric(vertical: 15.0), // 상하 패딩
+      child: Row(
+        children: [
+          if (showBackButton)
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              color: const Color(0xFFFB5457),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          Expanded(
+            child: Align(
+              alignment:
+                  title == "안심하이" ? Alignment.centerLeft : Alignment.center,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center, // 텍스트와 이미지 중앙 정렬
+                children: [
+                  Transform.translate(
+                    offset: const Offset(0, 3), // Y축으로 3픽셀 아래로 이동
+                    child: Image.asset(
+                      'assets/images/logoicon.png', // 이미지 경로
+                      width: 30, // 이미지 크기 설정
+                      height: 30,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  const SizedBox(width: 2), // 이미지와 텍스트 사이 간격
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      color: Color(0xFFFB5457), // 텍스트 색상
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
